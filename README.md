6245440cf34a7a130ac6de6f148996b10c56c2d9
Florent Piétot <florent.pietot@gmail.com>
2015-12-08 15:21:39 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bubble
+
+Wireframing for Bubble's website
---
+++ js/creative.js
@@ -0,0 +1,24 @@
+/*!
+ * Start Bootstrap - Creative Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+(function($) {
+    "use strict"; // Start of use strict
+    
+    // Fit Text Plugin for Main Header
+    $("h1").fitText(
+        1.2, {
+            minFontSize: '35px',
+            maxFontSize: '65px'
+        }
+    );
+
+    // Offset for Main Navigation
+    // $('#mainNav').affix({
+    //     offset: {
+    //         top: 100
+    //     }
+    // })
+})(jQuery); // End of use strict
